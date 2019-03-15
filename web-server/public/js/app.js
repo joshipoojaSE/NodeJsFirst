beb56b15d66d0557d2cd2ad3b2ce5c7ad2a75703
joshipoojaSE <pj.official.se225@gmail.com>
2019-03-15 14:32:26 +0530

    Add high and low to forecast

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,13 +3,14 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-messageOne.textContent = 'Loading Message...';
-messageTwo.textContent = '';
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const location =search.value;
+    messageOne.textContent = 'Loading Message...';
+    messageTwo.textContent = '';
+
+    const location = search.value;
 
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
@@ -20,6 +21,6 @@ weatherForm.addEventListener('submit', (e) => {
                 messageTwo.textContent = data.forecast;
             }
         })
-    }); 
+    });
 });
 
